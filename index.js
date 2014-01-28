067639486ed272ccef218660feb291e8a22e3a81
David McMullin <david.mcmullin@gmail.com>
2014-01-28 20:35:10 +0000

    find result by polling with setTimeout

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var Q = require('q');
 
 var doTheStuff = function(ajax, url, callback){
-  ajax(url)
+  var result = ajax(url)
+  function checkForResult(){
+    if(result.data) {
+      console.log(result.data);
+    } else {
+      setTimeout(checkForResult, 10);
+    }
+  }
+  checkForResult();
 };
 
 module.exports = doTheStuff;
