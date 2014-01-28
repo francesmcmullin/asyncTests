adaa0b83522c59298996680efff4b4fc68fb2b2e
David McMullin <david.mcmullin@gmail.com>
2014-01-28 21:02:47 +0000

    fix test 8, call a callback with the results

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var Q = require('q');
 
 var doTheStuff = function(ajax, url, callback){
-  ajax(url, function(error, result){
-    console.log(error || result.data);
+  callback = callback || function(){};
+  ajax(url, function(error, result1){
+    console.log(error || result1.data);
     if(error){return}
 
-    ajax(result.url1, function(error, result){
-      console.log(error || result.data);
+    ajax(result1.url1, function(error, result2){
+      console.log(error || result2.data);
+      if(error){return}
+
+      callback(null, result1.data + result2.data);
     })
   })
 };
