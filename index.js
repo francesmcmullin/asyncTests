6155dab22be798703e5a40754e682b9e8a48b23c
David McMullin <david.mcmullin@gmail.com>
2014-01-28 21:59:57 +0000

    fix test 8, invoke the callback with both results

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var Q = require('q');
 
 var doTheStuff = function(ajax, url, callback){
+  var finalMessage = '';
   return ajax(url).then(function(result){
     console.log(result.data);
+    finalMessage += result.data;
     return ajax(result.url1);
   })
   .then(function(result){
     console.log(result.data);
+    finalMessage += result.data;
+    return finalMessage;
   })
   .then(null, function(error){
     console.log(error);
     throw error;
-  });
+  }).nodeify(callback);
 };
 
 module.exports = doTheStuff;
