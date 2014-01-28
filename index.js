9a128386945278d2cfacb7d252fa7e2659b061c0
David McMullin <david.mcmullin@gmail.com>
2014-01-28 21:51:28 +0000

    fix tests 1,2 and 3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Q = require('q');
 
 var doTheStuff = function(ajax, url, callback){
-
+  return ajax(url).then(function(result){
+    console.log(result.data);
+  });
 };
 
 module.exports = doTheStuff;
