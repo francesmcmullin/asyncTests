d9a74af637c5172ca3be3c25ddb322559f27dd80
David McMullin <david.mcmullin@gmail.com>
2014-01-28 20:43:04 +0000

    fix test 6 and 7, log an error instead of data, exit if error

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var Q = require('q');
 
 var doTheStuff = function(ajax, url, callback){
   ajax(url, function(error, result){
-    console.log(result.data);
+    console.log(error || result.data);
+    if(error){return}
 
     ajax(result.url1, function(error, result){
       console.log(error || result.data);
