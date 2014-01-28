0d04402c97618076e296e6781a1fec09ed68797a
David McMullin <david.mcmullin@gmail.com>
2014-01-28 21:55:24 +0000

    fix tests 4 and 7, make the second fetch

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var Q = require('q');
 var doTheStuff = function(ajax, url, callback){
   return ajax(url).then(function(result){
     console.log(result.data);
+    return ajax(result.url1);
+  })
+  .then(function(result){
+    console.log(result.data);
   });
 };
 
